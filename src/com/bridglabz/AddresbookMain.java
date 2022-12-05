7b7b1dbb6587d8d5409041818bced04ee6503ca2
akshayzagad <akshayzagade62@gmail.com.com>
2022-12-05 19:41:51 +0530

    Added ; Created Contact of Person

--- src/com/bridglabz/AddresbookMain.java
+++ src/com/bridglabz/AddresbookMain.java
@@ -1,7 +1,23 @@
 package com.bridglabz;
 
+import java.util.Scanner;
+
 public class AddresbookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Addresbook Program");
+        System.out.println("WelCome To AddressBook Program");
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the first name");
+        String  fName = sc.next();
+        System.out.println("Enter the last Name");
+        String lName = sc.next();
+        System.out.println("Enter the city");
+        String City = sc.next();
+        System.out.println("Enter the Address");
+        String Address = sc.next();
+        System.out.println("Enter the number");
+        int Number = sc.nextInt();
+        System.out.println("Enter the mail");
+        String Email = sc.next();
     }
 }
